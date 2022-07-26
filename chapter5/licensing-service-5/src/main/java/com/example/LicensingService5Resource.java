9e0236462957e3a1d1a0616cd120357f73e40738
kuanghc1 <kuanghc1@lenovo.com>
2022-07-26 14:18:00 +0800

    Add exceptions

--- chapter5/licensing-service-5/src/main/java/com/example/LicensingService5Resource.java
+++ chapter5/licensing-service-5/src/main/java/com/example/LicensingService5Resource.java
@@ -2,10 +2,15 @@ package com.example;
 
 import com.example.entity.Fruit;
 import com.example.entity.License;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.quarkus.hibernate.reactive.panache.Panache;
 import io.quarkus.panache.common.Parameters;
+import io.smallrye.mutiny.CompositeException;
 import io.smallrye.mutiny.Uni;
+import org.jboss.logging.Logger;
 
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -14,8 +19,11 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 import java.util.List;
 import java.util.UUID;
 
@@ -26,6 +34,8 @@ import static javax.ws.rs.core.Response.Status.NO_CONTENT;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class LicensingService5Resource {
+
+    private static final Logger LOGGER = Logger.getLogger(LicensingService5Resource.class.getName());
 
     @GET
     @Path("{licenseId}")
@@ -52,6 +62,9 @@ public class LicensingService5Resource {
 
     @POST
     public Uni<Response> createLicense(License license, @PathParam("organizationId") String organizationId) {
+        if (license == null) {
+            throw new WebApplicationException("license can't be null when creating!");
+        }
         license.setLicenseId(UUID.randomUUID().toString());
         license.setOrganizationId(organizationId);
         return Panache
@@ -77,5 +90,42 @@ public class LicensingService5Resource {
                         .map(deleted -> deleted > 0
                                 ? Response.ok().status(NO_CONTENT).build()
                                 : Response.ok().status(NOT_FOUND).build()));
+    }
+
+    @Provider
+    public static class ErrorMapper implements ExceptionMapper<Exception> {
+
+        @Inject
+        ObjectMapper objectMapper;
+
+        @Override
+        public Response toResponse(Exception exception) {
+            LOGGER.error("Failed to handle request", exception);
+
+            Throwable throwable = exception;
+
+            int code = 500;
+            if (throwable instanceof WebApplicationException) {
+                code = ((WebApplicationException) exception).getResponse().getStatus();
+            }
+
+            // This is a Mutiny exception and it happens, for example, when we try to insert a new
+            // fruit but the name is already in the database
+            if (throwable instanceof CompositeException) {
+                throwable = throwable.getCause();
+            }
+
+            ObjectNode exceptionJson = objectMapper.createObjectNode();
+            exceptionJson.put("exceptionType", throwable.getClass().getName());
+            exceptionJson.put("code", code);
+
+            if (exception.getMessage() != null) {
+                exceptionJson.put("error", throwable.getMessage());
+            }
+
+            return Response.status(code)
+                    .entity(exceptionJson)
+                    .build();
+        }
     }
 }
